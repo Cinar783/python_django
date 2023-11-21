13202611c23c53e77ad7322edcd5b94e286a8813
Seyithan Çınar <seyithan.cinarr@gmail.com>
2023-11-21 13:31:34 +0300

    dinamik routing added

--- django_learning_project/first_app/views.py
+++ django_learning_project/first_app/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+course_dictoanry={
+    "Python":"Python Course Page",
+    "Jav":"Java Course Page",
+    "Kotlin":"Kotlin Course Page",
+    "Swift":"Swift Course Page"
+}
+
 def index(request):
     return HttpResponse("This is first Django procets first index")
+
+def course(request,item):
+    return HttpResponse(course_dictoanry.get(item, "Not Fount"))
+
